f374425f87de2326423ff6af23396043fdbff26e
WaffleChampion <bendik1234@hotmail.no>
2019-05-29 12:52:34 +0200

    changable list

--- code/homepage/tablepage/index.js
+++ code/homepage/tablepage/index.js
@@ -1,32 +1,47 @@
 function addToIdea(){
     var card=document.createElement("li");
     var cardList=document.createElement("ul");
-    var cardListElement=document.createElement("li");
     var input = prompt("Enter card name");
+    var cardListElement=document.createElement("li");
     var textNode =document.createTextNode(input);
     cardListElement.appendChild(textNode);
     document.getElementById("ideaList").appendChild(card);
     card.appendChild(cardList);
     cardList.appendChild(cardListElement);
-    
+    var cardNameAtt=document.createAttribute("id");
+    cardNameAtt.value=input;
+    cardListElement.setAttributeNode(cardNameAtt);
     var description = prompt("Add a description");
     if(description!=null){
         var descriptionItem=document.createElement("li");
         var descriptionNode=document.createTextNode(description);
+        var desNameAtt = document.createAttribute("id");
+        desNameAtt.value=prompt("add an id");
         descriptionItem.appendChild(descriptionNode);
         cardList.appendChild(descriptionItem);
+        descriptionItem.setAttributeNode(desNameAtt);
     }
     
     var users = prompt("Add users to project, seperate by comma");
     if(users!=null){
         var userItem=document.createElement("li");
         var userNode=document.createTextNode(users);
+        var usrNameAtt = document.createAttribute("id");
+        usrNameAtt.value=prompt("add an id");
         userItem.appendChild(userNode);
         cardList.appendChild(userItem);
+        userItem.setAttributeNode(usrNameAtt);
     }
     cardList.style.border="1px solid black";
     
 }
+document.addEventListener('click', function(e) {
+    var element= document.getElementById(e.target.id);
+    //alert(element.tagName);
+    if(element.tagName == "LI"){
+    var newData = prompt("enter new data");   document.getElementById(e.target.id).innerHTML=newData;
+}
+}, false);
 
 function addToDoList(){
     var card=document.createElement("li");
@@ -37,6 +52,7 @@ function addToDoList(){
     cardListElement.appendChild(textNode);
     document.getElementById("todoList").appendChild(card);
     card.appendChild(cardList);
+    cardList.appendChild(cardListElement);
     var description = prompt("Add a description");
     if(description!=null){
         var descriptionItem=document.createElement("li");
@@ -65,7 +81,7 @@ function addToProgress(){
     cardListElement.appendChild(textNode);
     document.getElementById("progressList").appendChild(card);
     card.appendChild(cardList);
-    document.getElementById("progressList").appendChild(card);
+    cardList.appendChild(cardListElement);
     var description = prompt("Add a description");
     if(description!=null){
         var descriptionItem=document.createElement("li");
@@ -82,6 +98,7 @@ function addToProgress(){
         cardList.appendChild(userItem);
     }
     cardList.style.border="1px solid black";
+ 
 }
 
 function addToFinished(){
@@ -93,6 +110,7 @@ function addToFinished(){
     cardListElement.appendChild(textNode);
     document.getElementById("finishedList").appendChild(card);
     card.appendChild(cardList);
+    cardList.appendChild(cardListElement);
     var description = prompt("Add a description");
     if(description!=null){
         var descriptionItem=document.createElement("li");
@@ -109,5 +127,6 @@ function addToFinished(){
         cardList.appendChild(userItem);
     }
     cardList.style.border="1px solid black";
+ 
 }
 
