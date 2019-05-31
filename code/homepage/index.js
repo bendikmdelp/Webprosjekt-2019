45268a55f41f982062c1bef25d54a6837ec6bf50
Ole Norholm <olenorholm55@gmail.com>
2019-05-31 12:31:50 +0200

    Startet på nytt

--- code/homepage/index.js
+++ code/homepage/index.js
@@ -1,5 +1,6 @@
-var month = "July"
 
-document.getElementById("h1").innerHTML = "July";
+var monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 
+//var s = document.getElementById("month");
+//s.textContent = prompt("Sk");
