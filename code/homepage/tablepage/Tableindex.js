f50336aa1147e7bbd0e8c00d3edcb287ca7c40ab
WaffleChampion <bendik1234@hotmail.no>
2019-06-03 11:20:14 +0200

    removed useless file

--- code/homepage/tablepage/Tableindex.js
+++ code/homepage/tablepage/Tableindex.js
@@ -140,7 +140,7 @@ function addToIdea(){
 document.addEventListener('click', function(e) {
     var element= document.getElementById(e.target.id);
     if(element.tagName === "LI"){
-    var newData = prompt("enter new data");   document.getElementById(e.target.id).innerHTML=newData;
+        var newData = prompt("enter new data");   document.getElementById(e.target.id).innerHTML=newData;
     }
 }, false);
 
