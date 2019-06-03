5b1d3a54a4b78be5a397fb9404e6f1fa701f4e02
WaffleChampion <bendik1234@hotmail.no>
2019-06-03 12:34:40 +0200

    File restructuring

---
+++ code/homepage/tablepage/preCodedData.js
@@ -0,0 +1,105 @@
+let tasks=[
+    {
+        id: 1,
+        name: "Creating a simple webapp",
+        dueDate: "2019-06-05",
+        description: "We need to create a simple webapp that fulfills the criteria"
+    },
+    {
+        id: 2,
+        name: "Finish app",
+        dueDate: "2019-06-05",
+        description: "We need to finish the app by the deadline"
+    },
+    {
+        id: 3,
+        name: "Creating cards",
+        dueDate: "2019-06-01",
+        description: "Need to create the card needed in the app"
+        
+    },
+    {
+        id:4,
+        name: "Download tools",
+        dueDate: "2019-05-29",
+        description: "Download the tools needed to complete the assignment"
+    }
+];
+
+let team = [
+
+    { id: 1, name: "Marge" },
+
+    { id: 2, name: "Lisa" },
+
+    { id: 3, name: "Homer" },
+
+    { id: 4, name: "Maggie" },
+
+];
+
+window.onload = function addToTask(){
+    for(var i=0; i<=tasks.length; i++){
+        //var card=document.createElement("li");
+        var cardList=document.createElement("ul");
+        var cardListElement=document.createElement("li");
+        cardList.setAttribute("id", tasks[i].name);
+       
+        if(tasks[i].id===1){
+            document.getElementById("ideaList").appendChild(cardList);
+        }
+         else if(tasks[i].id===2){
+            document.getElementById("todoList").appendChild(cardList);
+        }else if(tasks[i].id===3){
+            document.getElementById("progressList").appendChild(cardList);
+        }else if(tasks[i].id===4){
+            document.getElementById("finishedList").appendChild(cardList);
+        }
+        cardList.setAttribute("draggable", true);
+        cardList.setAttribute("ondragstart","drag(event)");
+        cardList.ondragstart=function(){drag(event)};
+        cardListElement.style.fontSize="30";
+        cardListElement.innerHTML=tasks[i].name;
+        
+        //card.appendChild(cardList);
+        cardList.appendChild(cardListElement);
+        
+        var cardNameAtt=document.createAttribute("id");
+        cardNameAtt.value=tasks[i].id;
+        cardListElement.setAttributeNode(cardNameAtt);
+        var descriptionItem=document.createElement("li");
+        descriptionItem.innerHTML=tasks[i].description;
+        cardList.appendChild(descriptionItem);
+        var desNameAtt = document.createAttribute("id");
+        desNameAtt.value="descrption";
+        descriptionItem.setAttributeNode(desNameAtt);
+        descriptionItem.style.fontSize="20";
+
+        cardList.style.border="1px solid black";
+        
+        var DueDate = "must be done by "+tasks[i].dueDate;
+        var dueDate=document.createElement("li");
+        dueDate.innerHTML=DueDate;
+        cardList.appendChild(dueDate);
+        dueDate.setAttribute("id", "due"+i);
+        dueDate.style.fontSize="30";
+        
+        var assigned = "Assigned to: "+team[i].name;
+        var assignedList=document.createElement("li");
+        assignedList.innerHTML=assigned;
+        cardList.appendChild(assignedList);
+        assignedList.setAttribute("id", "users"+i);
+        assignedList.style.fontSize="30";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
