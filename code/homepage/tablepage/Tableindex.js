30bd9a603be77002d5cf347f09f0d658c76b97bc
WaffleChampion <bendik1234@hotmail.no>
2019-06-03 11:03:42 +0200

    Added chat function

--- code/homepage/tablepage/Tableindex.js
+++ code/homepage/tablepage/Tableindex.js
@@ -315,3 +315,23 @@ function addToFinished(){
     cardList.style.border="1px solid black";
     
 }
+
+function showChat(){
+    var style = document.querySelector("#chatbox")
+    var chatBox = document.getElementById("chatbox");
+    var chatBoxStyle =getComputedStyle(style);
+    if(chatBoxStyle.bottom=="-200px"){
+        chatBox.style.bottom="0";
+    }else{
+        chatBox.style.bottom="-200px";
+    }
+    chatBox.style.transition="2s";
+}
+
+function sendChat(){
+    var chatItem=document.createElement("li");
+    var message=document.getElementById("chatInput").value;
+    var chatNode=document.createTextNode(message);
+    chatItem.appendChild(chatNode);
+    document.getElementById("chatList").appendChild(chatItem);
+}
