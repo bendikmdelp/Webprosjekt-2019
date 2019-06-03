e2b377783fa8583eada34eebe3250165b7b3cf00
Vilgeir <vilgeir@gmail.com>
2019-06-03 15:48:48 +0200

    test

--- code/homepage/tablepage/Changingdata.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('click', function(e) {
-    var element= document.getElementById(e.target.id);
-    if(element.tagName === "LI"){
-        var newData = prompt("enter new data");   document.getElementById(e.target.id).innerHTML=newData;
-    }
-}, false);
