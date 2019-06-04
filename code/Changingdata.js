a5b9e171e1e773ae1fd3ec6d410f8d3d37818970
WaffleChampion <bendik1234@hotmail.no>
2019-06-04 12:32:27 +0200

    code commenting and improved user selection

--- code/Changingdata.js
+++ code/Changingdata.js
@@ -1,3 +1,4 @@
+//evenlistener with function to change the data of an <li> element when clicked on
 document.addEventListener('click', function(e) {
     var element= document.getElementById(e.target.id);
     if(element.tagName === "LI"){
--- code/Chat.js
+++ code/Chat.js
@@ -1,7 +1,9 @@
+//function to show the chatbox when the button is clicked on
 function showChat(){
     var style = document.querySelector("#chatbox")
     var chatBox = document.getElementById("chatbox");
     var chatBoxStyle =getComputedStyle(style);
+    //the next if/else statement is to make the the chatbox move up if it is out of view, and back if it is in view
     if(chatBoxStyle.bottom=="-200px"){
         chatBox.style.bottom="0";
     }else{
@@ -10,6 +12,7 @@ function showChat(){
     chatBox.style.transition="2s";
 }
 
+//function to add the text in the input field to the chat box
 function sendChat(){
     var chatItem=document.createElement("li");
     var message=document.getElementById("chatInput").value;
