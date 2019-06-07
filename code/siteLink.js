6673b95760dcc5fd6684d1b0fc37049d921610ae
WaffleChampion <bendik1234@hotmail.no>
2019-06-07 13:14:37 +0200

    Update siteLink.js

--- code/siteLink.js
+++ code/siteLink.js
@@ -12,7 +12,7 @@ function linkInfo(){
 }
 
 function linkCalendar(){
-    window.location.href = "../Calendar/calendar.html";
+    window.location.href = "../calendar/calendar.html";
 }
 
 function linkTable(){
